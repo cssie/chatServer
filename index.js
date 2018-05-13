25633d0899edba21898ef14d7e3b2277495331c0
cassie <1411674262@qq.com>
2018-05-13 16:52:28 +0800

    测试聊天功能

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const mongoose = require('mongoose');
-const Koa = require('koa');
-const app = new Koa();
-const route = require('koa-route');
-
-const server = require('http').createServer(app.callback());
-const io = require('socket.io')(server);
-
-
-io.on('connection', function(socket){
-  console.log('ddddd')
-  socket.emit('request', {},function (res) {
-    console.log(res,'我服务端出发了事件啦')
-  }); // emit an event to the socket
-  socket.on('reply', function(res){
-    console.log(res,'好的好的我知道客户端收到信息啦')
-  }); // listen to the event
-  socket.on('error',function (err) {
-      console.log(err)
-    }
-  )
-});
-
-
+const mongoose = require('mongoose')
+const koa = require('koa')
+const app = new koa();
+var server = require('http').createServer(app.callback());
+var io = require('socket.io')(server);
+const cors = require('koa2-cors');
+app.use(cors())
+//socket
+io.on('connection',function (socket) {
+    console.log('我连上啦啦啦啦');
+    socket.on('requestMessage',function (data) {
+        console.log(data)
+        socket.emit('sendMessage',{fromID:data.fromID,toID:data.toID,message:data.message});
+    })
+})
+server.listen(3000);
 //连接数据库
 mongoose.connect('mongodb://erbai:huangNan19940101@120.79.138.96:27017/nodeTest?authSource=admin');
 // 连接成功
@@ -38,27 +30,3 @@ mongoose.connection.on('error', (err) => {
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose connection disconnected');
 });
-
-// request && response
-const about = ctx => {
-  ctx.response.type = 'json';
-  ctx.response.body = {data:'ddd'};
-};
-
-const main = ctx => {
-  ctx.response.body = 'Hello World';
-};
-
-app.use(route.get('/', main));
-app.use(route.get('/about', about));
-
-
-server.listen(4038);
-server.on('error',function (err) {
-  console.log('serverError',err)
-})
-server.on('listening',function (listen) {
-  console.log('listening',listen)
-})
-
-// app.listen(3000);
